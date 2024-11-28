337e53d0070a28eb8849bd879a328fefff4bbe75
Robert Bubnik <rbubnik@gmail.com>
2024-11-27 22:05:24 -0500

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat (newCat) {
+    cats.push(newCat)
+}
+
+function destructivelyPrependCat (newCat) {
+    cats.unshift(newCat)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(-1);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0)
+}
+
+function appendCat (newCat) {
+    let newList = [...cats, newCat]
+    return newList
+}
+
+function prependCat (newCat) {
+    let newList = [newCat, ...cats]
+    return newList
+}
+
+function removeLastCat () {
+    let newList = cats.slice(0,-1)
+    return newList
+}
+
+function removeFirstCat () {
+    let newList = cats.slice(1)
+    return newList
+}
+
+
